a2dd4a1a67fb1f1943151b262639630dabf037d4
hugues0 <ntwari.hugues@gmail.com>
2021-07-17 22:06:58 +0200

    preparing for deployment

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+import env from 'dotenv'
 import express from 'express'
 import cors from 'cors'
 import path from 'path'
 import route from './server/routes/albumRoute.js'
 
+env.config();
 const app = express()
 app.use(cors())
 app.use('/api/v1', route)
+app.get('/',(req,res) => {
+    res.send('Welcome to challenge three!!!')
+})
 
-const port = 10000;
+const port = process.env.PORT || 10000;
 app.listen(port,() => console.log(`App listening on port ${port}`));
